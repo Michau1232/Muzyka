ee3f3d84d0ac5ea3b6e0faec9d1e2ae79cce28cf
Michau123456 <65906312+Michau123456@users.noreply.github.com>
2020-06-09 13:44:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { TOKEN, PREFIX } = require("./config.json")
 //CLIENT EVENTS
 client.on("ready", () => {
   console.log(`Zalogowano jako ${client.user.tag}`),
-  client.user.setActivity(`❓${prefix}mhelp`); 
+  client.user.setActivity(`❓ <mhelp`); 
 })
 
 
