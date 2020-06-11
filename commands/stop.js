023654f447e833a7844d7adb6d02914610f9669e
Michau123456 <65906312+Michau123456@users.noreply.github.com>
2020-06-11 18:52:24 +0200

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,5 +1,6 @@
-const discord = require("discord.js");
 if(!message.member.hasPermission('ADMINISTRATOR')) {return message.reply("Wystąpił jakiś problem z komendą! Powiadom administratora!")}
+const discord = require("discord.js");
+
 module.exports = {
   name: "stop",
   description: "Stop the music",
